4632b5c41a582704f51ec94ff9d2df0153a7ffae
Brian Demers <bdemers@apache.org>
2017-10-16 12:58:44 -0400

    Add login page support

--- src/main/java/com/example/oauthimplicitexample/DemoApplication.java
+++ src/main/java/com/example/oauthimplicitexample/DemoApplication.java
@@ -2,7 +2,10 @@ package com.example.oauthimplicitexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @EnableResourceServer
 @SpringBootApplication
@@ -10,5 +13,17 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	protected ResourceServerConfigurerAdapter resourceServerConfigurerAdapter() {
+		return new ResourceServerConfigurerAdapter() {
+			@Override
+			public void configure(HttpSecurity http) throws Exception {
+				http.authorizeRequests()
+						.antMatchers("/", "/index.html", "/sign-in-widget-config").permitAll()
+						.anyRequest().authenticated();
+			}
+		};
 	}
 }
---
+++ src/main/java/com/example/oauthimplicitexample/SignInWidgetConfigController.java
@@ -0,0 +1,43 @@
+package com.example.oauthimplicitexample;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+public class SignInWidgetConfigController {
+
+    private final String issuerUrl;
+    private final  String clientId;
+
+    public SignInWidgetConfigController(@Value("#{@environment['okta.oauth2.clientId']}") String clientId,
+                                        @Value("#{@environment['okta.oauth2.issuer']}")   String issuerUrl) {
+
+        Assert.notNull(clientId,  "Property 'okta.oauth2.clientId' is required.");
+        Assert.notNull(issuerUrl, "Property 'okta.oauth2.issuer' is required.");
+        this.clientId = clientId;
+        this.issuerUrl = issuerUrl;
+    }
+
+    @GetMapping("/sign-in-widget-config")
+    public WidgetConfig getWidgetConfig() {
+        return new WidgetConfig(issuerUrl, clientId);
+    }
+
+    public static class WidgetConfig {
+        public String baseUrl;
+        public String clientId;
+        public Map<String, Object> authParams = new LinkedHashMap<>();
+
+        WidgetConfig(String issuer, String clientId) {
+
+            this.clientId = clientId;
+            this.authParams.put("issuer", issuer);
+            this.baseUrl = issuer.replaceAll("/oauth2/.*", "");
+        }
+    }
+}
